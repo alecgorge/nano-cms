d6294517caf93a97bc7b955492517677c4d41dde
alecgorge <alecgorge@385bc71e-76f3-11de-bf7b-bff11111869b>
2009-07-22 19:22:40 +0000

    committing old stable versions of nanocms

---
+++ trunk/0.1/data/intro.php
@@ -0,0 +1,32 @@
+<h2>About NanoCMS v0.1</h2>
+<p>
+	Nano CMS is the tinyest CMS you can find around or the tinyest that i could have made :)<br>
+	The user interface and the functionality are very very simple and easy to use.<br><br>
+</p>
+<p>
+	Through this CMS you can create only pages without actually touching editors or anything.<br>
+	You can manage pages which appear in sidebar and some simple pages which you might want to link saperately<br>
+	nano cms is simple and neat enough to manage websites with static content through an easy to use interface.<br>
+	If you dont like the design it is very easy to integrate new designs also...
+</p>
+
+<h2>How to create pages</h2>
+<p>
+	Creating pages is very simple..just follow these steps
+	<ul>
+		<li>Click on 'Add page'</li>
+		<li>Enter the title of page</li>
+		<li>Select weather it should be displayed in sidebar or not</li>
+		<li>Enter the content</li>
+		<li>Click Submit and viola the page is created</li>
+	</ul>
+</p>
+<h2>v0.1 Limitations / ToDo</h2>
+<p>
+	This release version has certain limitations in its capabilities...These would be improved in upcoming releases..so watchout
+	<ul>
+		<li>You cannot edit page title's and their locations</li>
+		<li>You gotta change the page elements like ( name of website etc ) manually...</li>
+		<li>no WYSIWYG editor</li>
+	</ul>
+</p>
---
+++ trunk/0.1/data/pages/contact.txt
@@ -0,0 +1,2 @@
+<h2>Contact</h2>
+<p>You can contact me at <a href='mailto:kalyan@KalyanChakravarthy.net'>kalyan@KalyanChakravarthy.net</a> for feed back, bug reports, tips, feature-requests etc et</p>
